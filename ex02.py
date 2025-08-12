c5ce28541cf3315a5453350702d862fd7e722a8f
bbun <wkoilz1028@gmail.com>
2025-08-12 09:10:58 +0900

    250811 At home review

--- ex02.py
+++ ex02.py
@@ -7,7 +7,7 @@ data = {"a": [80, 90, 70, 30], "b": [90, 70, 60, 40], "c": [90, 60, 80, 70]}
 1) 모든 학생의 수학 점수를 출력하기
 2) 모든 학생의 수학 점수의 표준편차를 출력하기
 3) 모든 학생의 국어와 영어 점수를 Series 타입이 아니라 DataFrame type으로 출력하기 (배점:10)
-'''
+
 from pandas import DataFrame
 import numpy as np
 
@@ -30,3 +30,12 @@ print(DataFrame(data))
 # print(df['수학'].std())
 
 print(df[['a','b']])
+
+'''
+a = 2
+b= 91
+
+def solution(a, b):    
+    return max(int(f'{a}{b}'),2*a*b)
+
+print(solution(a,b))
